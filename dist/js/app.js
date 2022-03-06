cc9a396eab48a8980bd065860be3bdedb6d96fc9
Hibarist <100476695+hibarist@users.noreply.github.com>
2022-03-05 23:03:41 -0300

    initial commit

---
+++ dist/js/app.js
@@ -0,0 +1,13 @@
+const contactButton = document.getElementById("contact-btn");
+const soundcloudBtn = document.getElementById("soundcloudBtn");
+const youtubeBtn = document.getElementById("youtubeBtn");
+
+contactButton.addEventListener("click", () => {
+    window.open("mailto: hibarist@gmail.com","_blank");
+})
+soundcloudBtn.addEventListener("click", () => {
+    window.open("https://soundcloud.com/hibarist","_blank");
+})
+youtubeBtn.addEventListener("click", () => {
+    window.open("https://youtube.com/c/hibarist/videos","_blank");
+})
