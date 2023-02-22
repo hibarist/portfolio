8bbe0751111f11565fd25d0a0a46ec75fdb01f58
Hibarist <100476695+hibarist@users.noreply.github.com>
2023-02-22 11:33:15 -0300

    Update app.js

--- dist/js/app.js
+++ dist/js/app.js
@@ -6,7 +6,7 @@ contactButton.addEventListener("click", () => {
     window.open("mailto: hibarist@gmail.com","_blank");
 })
 soundcloudBtn.addEventListener("click", () => {
-    window.open("https://soundcloud.com/hibarist","_blank");
+    window.open("https://soundcloud.com/hibarist/tracks","_blank");
 })
 youtubeBtn.addEventListener("click", () => {
     window.open("https://youtube.com/c/hibarist/videos","_blank");
